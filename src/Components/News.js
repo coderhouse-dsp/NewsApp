c6ac7319a5d325e59f7e012016482a1f20486f62
coderhouse-dsp <dspdeep222@gmail.com>
2022-02-22 18:49:46 +0530

    Day25completed

--- src/Components/News.js
+++ src/Components/News.js
@@ -8,7 +8,12 @@ export class News extends Component {
         <h2>NewsMonkey - Top Headlines</h2>
         <div className='row'>
           <div className='col-md-3'>
-            <NewsItem title='myTitle' description='mydesc' />
+            <NewsItem
+              title='myTitle'
+              description='mydesc'
+              imageUrl='https://ichef.bbci.co.uk/live-experience/cps/624/cpsprodpb/AF22/production/_123143844_langer1.jpg'
+              newsUrl='TODO'
+            />
           </div>
           <div className='col-md-3'>
             <NewsItem title='myTitle' description='mydesc' />
