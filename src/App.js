cc7bd19dc938fac8c9bb434ad54918617fe96d3e
coderhouse-dsp <dspdeep222@gmail.com>
2022-04-26 16:23:21 -0700

    Day33_completed

---
+++ src/App.js
@@ -0,0 +1,107 @@
+import './App.css'
+
+import React, { Component } from 'react'
+import Navbar from './Components/Navbar'
+import News from './Components/News'
+// import NewsItem from './Components/NewsItem'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+
+export default class App extends Component {
+  pageSize = 15;
+  render() {
+    return (
+      <div>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route
+              exact
+              path='/'
+              element={
+                <News
+                  pageSize={this.pageSize}
+                  key='general'
+                  country='in'
+                  category='general'
+                />
+              }
+            ></Route>
+            <Route
+              exact
+              path='/business'
+              element={
+                <News
+                  pageSize={this.pageSize}
+                  key='business'
+                  country='in'
+                  category='business'
+                />
+              }
+            ></Route>
+            <Route
+              exact
+              path='/entertainment'
+              element={
+                <News
+                  pageSize={this.pageSize}
+                  key='entertainment'
+                  country='in'
+                  category='entertainment'
+                />
+              }
+            ></Route>
+            <Route
+              exact
+              path='/health'
+              element={
+                <News
+                  key='health'
+                  pageSize={this.pageSize}
+                  country='in'
+                  category='health'
+                />
+              }
+            ></Route>
+            technology
+            <Route
+              exact
+              path='/science'
+              element={
+                <News
+                  key='science'
+                  pageSize={this.pageSize}
+                  country='in'
+                  category='science'
+                />
+              }
+            ></Route>
+            <Route
+              exact
+              path='/sports'
+              element={
+                <News
+                  key='sports'
+                  pageSize={this.pageSize}
+                  country='in'
+                  category='sports'
+                />
+              }
+            ></Route>
+            <Route
+              exact
+              path='/technology'
+              element={
+                <News
+                  pageSize={this.pageSize}
+                  key='technology'
+                  country='in'
+                  category='technology'
+                />
+              }
+            ></Route>
+          </Routes>
+        </Router>
+      </div>
+    )
+  }
+}
---
+++ src/Components/News.js
@@ -0,0 +1,107 @@
+import React, { Component } from 'react'
+import NewsItem from './NewsItem'
+import Spinner from './Spinner'
+import PropTypes from 'prop-types'
+
+export class News extends Component {
+  static defaultProps = {
+    country: 'in',
+    pageSize: 2,
+    category: 'general',
+  }
+  static propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string,
+  }
+  constructor() {
+    super()
+    console.log('Hello I am a constructor from News component')
+    this.state = {
+      articles: [],
+      loading: false,
+      page: 1,
+    }
+  }
+  async updateNews(pageNo)
+  {
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=52f2b93ef9764c26a72e94eab22c4790&page=${this.state.page}&pageSize=${this.props.pageSize}`
+    this.setState({ loading: true })
+    let data = await fetch(url)
+    let parsedData = await data.json()
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+      loading: false,
+    })
+  }
+  async componentDidMount() {
+    this.updateNews();
+  }
+  handleNextClick = async () => {
+    console.log('Next')
+    this.setState({page:this.setState.page+1});
+    this.updateNews();
+  }
+  handlePrevClick = async () => {
+    console.log('Next')
+    this.setState({page:this.setState.page-1});
+    this.updateNews();
+  }
+  render() {
+    console.log('I am render')
+    return (
+      <div className='container my-3'>
+        <h1 className='text-center'>NewsMonkey - Top Headlines</h1>
+        {this.state.loading && <Spinner />}
+        <div className='row'>
+          {/* {this.state.articles[0].url} */}
+          {!this.state.loading &&
+            this.state.articles.map((element) => {
+              return (
+                <div className='col-md-4' key={element.url}>
+                  <NewsItem
+                    title={element.title ? element.title.slice(0, 45) : ''}
+                    description={
+                      element.description
+                        ? element.description.slice(0, 88)
+                        : ''
+                    }
+                    imageUrl={element.urlToImage}
+                    newsUrl={element.url}
+                    author={element.author}
+                    date={element.publishedAt}
+                    source={element.source.name}
+                  />
+                </div>
+              )
+            })}
+        </div>
+        <div className='container d-flex justify-content-between'>
+          <button
+            disabled={this.state.page <= 1}
+            type='button'
+            className='btn btn-dark'
+            onClick={this.handlePrevClick}
+          >
+            {' '}
+            &larr; Previous
+          </button>
+          <button
+            disabled={
+              this.state.page + 1 >
+              Math.ceil(this.state.totalResults / this.props.pageSize)
+            }
+            type='button'
+            className='btn btn-dark'
+            onClick={this.handleNextClick}
+          >
+            Next &rarr;
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default News
